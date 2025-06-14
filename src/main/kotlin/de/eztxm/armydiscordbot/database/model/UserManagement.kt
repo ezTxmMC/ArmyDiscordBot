3d7df8ed4ee5154149ae5acec9d67602754adf21
ezTxmMC <eztxmmc@hotmail.com>
2025-06-14 14:06:25 +0200

    try to fix database issue

--- src/main/kotlin/de/eztxm/armydiscordbot/database/model/UserManagement.kt
+++ src/main/kotlin/de/eztxm/armydiscordbot/database/model/UserManagement.kt
@@ -5,7 +5,7 @@ import de.eztxm.armydiscordbot.database.Database
 class UserManagement(internal val database: Database) {
 
     fun createTable() {
-        database.put("CREATE TABLE IF NOT EXISTS users(id VARCHAR(75) PRIMARY KEY, xp BIGINT DEFAULT 0, cookieCount BIGINT DEFAULT 0);")
+        database.sqlConnection.put("CREATE TABLE IF NOT EXISTS users(id VARCHAR(75) PRIMARY KEY, xp BIGINT DEFAULT 0, cookieCount BIGINT DEFAULT 0);")
     }
 
     fun addUser(id: String?) {
@@ -14,7 +14,7 @@ class UserManagement(internal val database: Database) {
             println("TE")
             return
         }
-        database.put("INSERT INTO users(id) VALUES (?);", id)
+        database.sqlConnection.put("INSERT INTO users(id) VALUES (?);", id)
     }
 
     fun removeUser(id: String?) {
@@ -22,15 +22,15 @@ class UserManagement(internal val database: Database) {
         if (userExist(id)) {
             return
         }
-        database.put("DELETE FROM users WHERE id = ?;", id)
+        database.sqlConnection.put("DELETE FROM users WHERE id = ?;", id)
     }
 
     fun userExist(id: String?): Boolean {
         requireNotNull(id)
         return try {
-            val resultSet = database.query("SELECT id FROM users WHERE id = ?;", id)
-            println(resultSet.isClosed)
-            resultSet.next()
+            val resultSet = database.sqlConnection.query("SELECT id FROM users WHERE id = ?;", id)
+            println(resultSet.next())
+            resultSet?.next() == true && resultSet.getString("id") == id
         } catch (e: Exception) {
             e.printStackTrace()
             false
